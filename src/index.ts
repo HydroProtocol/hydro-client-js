9dc1a5a12061a7b62011f4ac7edfe8bb3dcd99ac
Kevin West <keevon@gmail.com>
2019-01-25 23:54:22 -0800

    Make sure to export all types in index

--- src/index.ts
+++ src/index.ts
@@ -2,21 +2,22 @@ import { HydroClient, HydroClientOptions } from './lib/HydroClient';
 import { HydroWatcher, HydroWatcherOptions, HydroListener } from './lib/HydroWatcher';
 
 import { Candle } from './models/Candle';
-import { Channel } from './models/Channel';
+import { Channel, ChannelName } from './models/Channel';
 import { Fee } from './models/Fee';
 import { LockedBalance } from './models/LockedBalance';
 import { Market } from './models/Market';
-import { Order } from './models/Order';
-import { Orderbook } from './models/Orderbook';
+import { Order, OrderType, Side, Status } from './models/Order';
+import { Orderbook, OrderbookLevel } from './models/Orderbook';
+import { OrderData } from './models/OrderData';
 import { PriceLevel } from './models/PriceLevel';
 import { Ticker } from './models/Ticker';
 import { Trade } from './models/Trade';
 import { TradeList } from './models/TradeList';
-import { OrderData } from './models/OrderData';
 
 export {
   Candle,
   Channel,
+  ChannelName,
   Fee,
   HydroClient,
   HydroClientOptions,
@@ -27,8 +28,12 @@ export {
   Market,
   Order,
   Orderbook,
+  OrderbookLevel,
   OrderData,
+  OrderType,
   PriceLevel,
+  Side,
+  Status,
   Ticker,
   Trade,
   TradeList,
